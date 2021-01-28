7c382ef9d66ff5db865fed539f933dd980d840d2
Karina <kserranoa16@gmail.com>
2021-01-28 03:31:10 -0600

    Product Exercise

---
+++ Product.py
@@ -0,0 +1,6 @@
+def calcProduct():
+    n1 = int(input("Enter number 1 \n >>> "))
+    n2 = int(input("Enter number 2 \n >>> "))
+    total = n1 * n2
+    result = print("The product of " , n1, "*", n2, " is: ", total)
+    return result
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import AreaCircle
 import conversion
 import Sum
+import Product
 
 # Shows result of the Circle Area
 #print(AreaCircle.calculateArea())
@@ -9,4 +10,7 @@ import Sum
 #print(conversion.toCelsius())
 
 #Shows result of Sum
-print(Sum.sum())
+#print(Sum.sum())
+
+#Obtain product of two numbers
+print(Product.calcProduct())
